4d4f2dcce1904007b72aaff9c1a492c397ee6597
mykkon <mykkon@gmail.com>
2022-08-08 22:54:48 +0200

    first commit

---
+++ calkulator.js
@@ -0,0 +1,11 @@
+
+  let n1 = prompt("Write down the first number", '');
+    let n2 = prompt("Write down the second number", '');
+
+ n1 = parseFloat(n1);
+
+ n2 = parseFloat(n2);
+
+ let result = n1 + n2;
+
+alert(result);
